2955c0ff1ce64cbe5f1b73fe4fa4c93523da6dc1
Unknown <heckmiika@hotmail.com>
2017-11-24 22:08:03 +0200

    meltable icecaps

---
+++ Assets/Scripts/MeltableBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MeltableBase : MonoBehaviour {
+
+	private float _startingHealth = 100;
+	private float _currentHealth;
+	private Vector3 _startingScale;
+	private MeshFilter _meshFilter;
+
+	void Start(){
+		_currentHealth = _startingHealth;
+		_startingScale = transform.localScale;
+		_meshFilter = GetComponent<MeshFilter> ();
+	}
+
+	public void AddHealth(float amount){
+		_currentHealth += amount;
+		UpdateSize ();
+	}
+
+	private void UpdateSize(){
+		Vector3 temp = transform.localScale;
+		temp.y = _startingScale.y * (_currentHealth / _startingHealth);
+		transform.localScale = temp;
+	}
+
+	public float GetRadius(){
+		return _meshFilter.mesh.bounds.size.x;
+	}
+}
