c61ce52425e9b1c4223f8fb00afd8949d5e4b0fe
Unknown <heckmiika@hotmail.com>
2017-11-24 22:37:04 +0200

    ice caps rotate to planet surface

--- Assets/Scripts/MeltableBase.cs
+++ Assets/Scripts/MeltableBase.cs
@@ -9,20 +9,37 @@ public abstract class MeltableBase : MonoBehaviour {
 	private Vector3 _startingScale;
 	private MeshFilter _meshFilter;
 
+	[SerializeField]
+	private Planet _planet;
+	private Vector3 _vectorToPlanet;
+
 	void Start(){
 		_currentHealth = _startingHealth;
 		_startingScale = transform.localScale;
 		_meshFilter = GetComponent<MeshFilter> ();
+
+		_vectorToPlanet = (_planet.transform.position - transform.position).normalized;
+		transform.rotation = Quaternion.LookRotation (-_vectorToPlanet);
+
 	}
 
 	public void AddHealth(float amount){
 		_currentHealth += amount;
 		UpdateSize ();
+
+		if (_currentHealth <= 0) {
+			Die ();
+		}
 	}
+
+	private void Die(){
+		Destroy (gameObject);
+	}
+		
 
 	private void UpdateSize(){
 		Vector3 temp = transform.localScale;
-		temp.y = _startingScale.y * (_currentHealth / _startingHealth);
+		temp.z = _startingScale.z * (_currentHealth / _startingHealth);
 		transform.localScale = temp;
 	}
 
