0a1ef1fdb9a296742d1aee2efb45652ad5cefee6
Jussi Joki <judejoki@gmail.com>
2017-11-25 19:55:09 +0200

    Explosion 1.0

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    void Explode()
+    {
+        var exp = GetComponent<ParticleSystem>();
+        var main = exp.main;
+        exp.Play();
+        Destroy(gameObject, main.duration);
+    }
+}
