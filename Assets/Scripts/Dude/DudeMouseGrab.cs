57ba77617981689b29d4f2d2e9185f2e39f27a34
Jussi Joki <judejoki@gmail.com>
2017-11-25 21:02:34 +0200

    Fixed dude not dragging correclt yif vive is attached

--- Assets/Scripts/Dude/DudeMouseGrab.cs
+++ Assets/Scripts/Dude/DudeMouseGrab.cs
@@ -7,17 +7,22 @@ public class DudeMouseGrab : MonoBehaviour
 
     public static DudeMouseGrab Instance { get; private set; }
 
+
+    private Camera _camera;
 	// Use this for initialization
 	void Start () {
         if (Instance) throw new Exception("Multiple DudeMouseGrab instances");
 	    Instance = this;
+
+	    _camera = GetComponentInChildren<Camera>();
+
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
+	void Update () {
 	    if (!Grabbed) return;
 	    if (Grabbed.State != Dude.DudeState.Grabbed) return;
-	    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	    var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
 	    RaycastHit hit;
 	    if (Physics.Raycast(ray, out hit)) {
