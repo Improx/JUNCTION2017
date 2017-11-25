1685b9ee791bbadce7e47e166a1ff09062e7156f
Unknown <heckmiika@hotmail.com>
2017-11-25 03:32:32 +0200

    hand moves mouse & smoke particles

---
+++ Assets/HandMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour {
+
+	private float _distanceFromCamera;
+
+	void Start(){
+		_distanceFromCamera = transform.position.z;
+	}
+
+	void Update(){
+		
+		var mousePos = Input.mousePosition;
+		mousePos.z = 10;
+		transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+	}
+}
